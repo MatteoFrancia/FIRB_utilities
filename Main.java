788d583ef4cb67c4eb5b00f306e104e61419d939
matteo.francia@gmail.com <shaker140585>
2015-02-10 11:02:05 +0100

    [added] check file type

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
 package com.big.firb;
 
+import java.io.File;
+
 public class Main {
+    static String FILE_DIR="C:\\Users\\matteo.francia3\\Desktop\\TestDir";
 
     public static void main(String[] args) {
-	//C:\Users\matteo.francia3\Desktop\TestDir
+        File currentDir=new File(FILE_DIR);
+
+        File[] dirElements=currentDir.listFiles();
+        for(int i=0;i<dirElements.length;i++){
+            System.out.println("["+i+"]"+dirElements[i]);
+            System.out.println("\t\t[IsDir] ==>"+dirElements[i].isDirectory());
+            System.out.println("\t\t[IsFile] ==>" + dirElements[i].isFile());
+            System.out.println("\t\t[IsZip] ==>" + (dirElements[i].getName().endsWith(".zip") || dirElements[i].getName().endsWith(".rar")));
+        }
+
+
 
     }
 }
