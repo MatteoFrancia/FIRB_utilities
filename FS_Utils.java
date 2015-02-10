437406303e340ddd689642fc22e47d29ecda0e60
matteo.francia@gmail.com <shaker140585>
2015-02-10 16:01:06 +0100

    [added] alpha version of the recursive directories scan

--- FS_Utils.java
+++ FS_Utils.java
@@ -1,15 +1,25 @@
 package com.big.firb;
 
-import java.io.File;
+import java.io.*;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class FS_Utils {
+    /**
+     * Hold the path of the directory to be scanned
+     */
     private static File ROOT_DIR;
+    /**
+     * The log file
+     */
+    private static String LOG_FILE="C:\\Users\\matteo.francia3\\Desktop\\testlog.txt";
 
     /**
      * Instantiate a new FS_Utils object to recursively scan fs dirs.
      * @param root_dir the host file system full path to the directory to scan
      */
-    public FS_Utils(String root_dir) {
+    public FS_Utils(String root_dir){
         try{
             ROOT_DIR=new File(root_dir);
         }
@@ -25,14 +35,71 @@ public class FS_Utils {
      */
     public String getRootDir()
     {
-        return ROOT_DIR.getName();
+        return ROOT_DIR.getAbsolutePath();
     }
 
-//    File[] dirElements=currentDir.listFiles();
-//    for(int i=0;i<dirElements.length;i++){
-//        System.out.println("["+i+"]"+dirElements[i]);
-//        System.out.println("\t\t[IsDir] ==>"+dirElements[i].isDirectory());
-//        System.out.println("\t\t[IsFile] ==>" + dirElements[i].isFile());
-//        System.out.println("\t\t[IsZip] ==>" + (dirElements[i].getName().endsWith(".zip") || dirElements[i].getName().endsWith(".rar")));
-//    }
+    /**
+     * Recursively scan the ROOT_DIR directory
+     * @throws IOException if the element is not found
+     */
+    public void scan() throws IOException {
+        File[] dirElements=ROOT_DIR.listFiles();
+
+        for(int i=0;i<dirElements.length;i++){
+            if((dirElements[i].getName().endsWith(".zip") || dirElements[i].getName().endsWith(".rar"))){
+                this.scanZip(dirElements[i]);
+            }
+            else if(dirElements[i].isDirectory()){
+                this.scan(dirElements[i].getAbsolutePath());
+            }else if(dirElements[i].isFile()){
+                //do nothing
+            }
+        }
+    }
+
+    /**
+     * Recursively scan the directory passed as a parameter to the method itself
+     * @param dir the folder to scan
+     * @throws IOException
+     */
+    public void scan(String dir) throws IOException {
+        File actualdir = ROOT_DIR=new File(dir);
+        File[] actualDirFiles = actualdir.listFiles();
+
+        for(int i=0;i<actualDirFiles.length;i++){
+            if((actualDirFiles[i].getName().endsWith(".zip") || actualDirFiles[i].getName().endsWith(".rar"))){
+                this.scanZip(actualDirFiles[i]);
+            }
+            else if(actualDirFiles[i].isDirectory()){
+                this.scan(actualDirFiles[i].getAbsolutePath());
+            }else if(actualDirFiles[i].isFile()){
+                //do nothing
+            }
+        }
+    }
+
+    /**
+     * Recursivey scan a zipped file
+     * @param file The zipped file
+     * @throws IOException if the file is not found
+     */
+    private void scanZip(File file) throws IOException{
+        ZipFile tmpZipFile = new ZipFile(this.getRootDir()+"\\"+file.getName());
+        Enumeration zipEntries = tmpZipFile.entries();
+
+        while (zipEntries.hasMoreElements()) {
+            this.logElement(((ZipEntry) zipEntries.nextElement()).getName());
+        }
+    }
+
+    /**
+     * Log to file element retrieved by the scanning routines
+     * @param element the element retrieved  (the one to log)
+     * @throws IOException if the log fil is not found
+     */
+    private void logElement(String element) throws IOException{
+        PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(LOG_FILE, true)));
+        writer.println(element);
+        writer.close();
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 package com.big.firb;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -7,17 +8,23 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner user_input = new Scanner( System.in );
-        System.out.print("[Enter root dir to search:] ");
+        System.out.print("Enter root dir to search (type \"1\" for default): ");
 
         FS_Utils fs_util;
 
-        if(user_input.next().equals("")){
+        if(user_input.next().equals("1")){
             fs_util=new FS_Utils(DEFAULT_DIR);
         }
         else{
             fs_util=new FS_Utils(user_input.next());
         }
 
+        try {
+            fs_util.scan();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //System.out.println(fs_util.getRootDir());
 
 
 
